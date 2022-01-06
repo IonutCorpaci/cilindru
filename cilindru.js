f1484c9c6784eb9213420ae22b95f0e5fb15a9a1
IonutCorpaci <corpaco2005@gmail.com>
2022-01-06 05:44:13 +0200

    first commit

---
+++ cilindru.js
@@ -0,0 +1,6 @@
+let raza = prompt('Introdu marimea razei');
+let inaltimea = prompt('Introu marimea inaltimei cilindrului');
+let pi = 3.14;
+
+var volum = raza * raza * inaltimea * pi ;
+alert('Volumul cilindrului este egal cu ' + volum);
